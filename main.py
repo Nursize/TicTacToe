73f7952d024509786d861c96f561f51a2ec9a4b1
Nursize <nursizebilen@gmail.com>
2020-01-29 11:50:37 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,95 @@
+#
+
+def boter_kaas_eieren():
+    print ('Boter Kaas & Eieren')
+    print ('Speler 1 begint met spelen, kiest een nummer tussen 1 en 9.')
+    print ()
+    board = [[1], [2], [3], [4], [5], [6], [7], [8], [9]]
+    end = False
+    win_commbinations = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
+
+#def draw: het teken van het board 
+    def draw():
+        print(board[0], board[1], board[2])
+        print(board[3], board[4], board[5])
+        print(board[6], board[7], board[8])
+        print()
+
+#def p1: een nummer kiezen vanuit board 
+    def p1():
+        n = choose_number()
+        if board[n] == "[X]" or board[n] == "[O]":
+            print("\nDat mag niet, kies een ander getal")
+            p1()
+        else:
+            board[n] = "[X]"
+
+#def p2: een nummer kiezen vanuit board 
+    def p2():
+        n = choose_number()
+        if board[n] == "[X]" or board[n] == "[O]":
+            print("\nDat mag niet, kies een ander getal")
+            p2()
+        else:
+            board[n] = "[O]"
+
+#def choose number: testen op goedenummer 
+#speler mogen niet de nummers kiezen die al gekozen zijn.
+    def choose_number():
+        while True:
+            while True:
+                a = input()
+                try:
+                    a  = int(a)
+                    a -= 1
+                    if a in range(0, 9):
+                        return a
+                    else:
+                        print("\nDat mag niet, kies een ander getal")
+                        continue
+                except ValueError:
+                   print("\nDat mag niet, kies een ander getal")
+                   continue
+
+#def check_board: testen op het board 
+#of een speler heeft gewonnen of het spel gelijk is 
+    def check_board():
+        count = 0
+        for a in win_commbinations:
+            if board[a[0]] == board[a[1]] == board[a[2]] == "[X]":
+                print("Speler 1 heeft gewonnen!\n")
+                print("Gefeliciteerd!\n")
+                return True
+
+            if board[a[0]] == board[a[1]] == board[a[2]] == "[O]":
+                print("Speler 2 heeft gewonnen!\n")
+                print("Gefelictieerd!\n")
+                return True
+        for a in range(9):
+            if board[a] == "[X]" or board[a] == "[O]":
+                count += 1
+            if count == 9:
+                print("Het is gelijkspel\n")
+                return True
+
+#while not end: om verder te kunnen spelen 
+    while not end:
+        draw()
+        end = check_board()
+        if end == True:
+            break
+        print("Speler 1 mag kiezen")
+        p1()
+        draw()
+        end = check_board()
+        if end == True:
+            break
+        print("Speler 2 mag kiezen")
+        p2()
+
+#if input: 
+    if input("Nog een keer? (J/N)\n") == "j":
+        print()
+        boter_kaas_eieren()
+
+boter_kaas_eieren()
